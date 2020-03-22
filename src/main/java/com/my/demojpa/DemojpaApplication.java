e0d895bbf262d8acd877184fc49f06fb855c605e
mengang-sys <377532225@qq.com>
2020-03-22 19:27:30 +0800

    [mengyang].6

--- src/main/java/com/my/demojpa/DemojpaApplication.java
+++ src/main/java/com/my/demojpa/DemojpaApplication.java
@@ -15,6 +15,7 @@ public class DemojpaApplication {
         System.out.println("3");
         System.out.println("4");
         System.out.println("5");
+        System.out.println("6");
     }
 
 }
