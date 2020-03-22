d493cf367bedfedaa8007b7d2c10505b31d293e7
mengang-sys <377532225@qq.com>
2020-03-22 19:04:37 +0800

    [mengyang].启动类2

--- src/main/java/com/my/demojpa/DemojpaApplication.java
+++ src/main/java/com/my/demojpa/DemojpaApplication.java
@@ -10,6 +10,7 @@ public class DemojpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemojpaApplication.class, args);
         System.out.println("1");
+        System.out.println("2");
     }
 
 }
