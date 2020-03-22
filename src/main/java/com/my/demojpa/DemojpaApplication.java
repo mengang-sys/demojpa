96164a733a324c59cf6bc5c8201156ffa926b948
mengang-sys <377532225@qq.com>
2020-03-22 19:09:22 +0800

    Revert "[mengyang].启动类1"
    This reverts commit b6d575c34c6a86d77a1a6f98156fde20e5b972e4.
    
    # Conflicts:
    #	.idea/workspace.xml
    #	src/main/java/com/my/demojpa/DemojpaApplication.java

--- src/main/java/com/my/demojpa/DemojpaApplication.java
+++ src/main/java/com/my/demojpa/DemojpaApplication.java
@@ -10,7 +10,6 @@ public class DemojpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemojpaApplication.class, args);
         System.out.println("1");
-        System.out.println("2");
     }
 
 }
