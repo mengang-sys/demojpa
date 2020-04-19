442a934da991827c3e939d8f0e5a73644e2eee4f
mengang-sys <377532225@qq.com>
2020-04-19 13:09:32 +0800

    [mengyang].reset.master.6

--- src/main/java/com/my/demojpa/reset/Reset.java
+++ src/main/java/com/my/demojpa/reset/Reset.java
@@ -7,5 +7,6 @@ public class Reset {
         System.out.println("reset.master.1");
         System.out.println("reset.master.4");
         System.out.println("reset.master.5");
+        System.out.println("reset.master.6");
     }
 }
