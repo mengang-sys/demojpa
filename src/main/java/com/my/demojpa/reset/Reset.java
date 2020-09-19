323f8eb59d31a0b956d16be0501a13da76e0e666
mengang-sys <377532225@qq.com>
2020-09-19 19:34:06 +0800

    [mengyang].3.4.5

--- src/main/java/com/my/demojpa/reset/Reset.java
+++ src/main/java/com/my/demojpa/reset/Reset.java
@@ -3,7 +3,11 @@ package com.my.demojpa.reset;
 public class Reset {
 
     public static void main(String[] args) {
-        System.out.println("dev");
-        System.out.println("reset.dev.2");
+        String a = "aaa";
+        String b = "aaa";
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        
+
     }
 }
