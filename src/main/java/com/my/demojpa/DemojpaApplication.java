ffa15fe64738ff595a703b2c75352a99baa515cb
mengang-sys <377532225@qq.com>
2020-11-16 22:35:31 +0800

    [mengyang].3.4.5

--- src/main/java/com/my/demojpa/DemojpaApplication.java
+++ src/main/java/com/my/demojpa/DemojpaApplication.java
@@ -10,11 +10,5 @@ public class DemojpaApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemojpaApplication.class, args);
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
     }
-
 }
